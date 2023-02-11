68b7ca9d48547b8a75fb06b01209a99367ac9c2c
Lex Li <support@lextm.com>
2023-02-11 15:38:32 -0500

    Fixed sitemap.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx_sitemap'
 ]
 
-html_baseurl = 'https://www.pysnmp.com/snmpfwd'
+html_baseurl = 'https://www.pysnmp.com/snmpfwd/'
 sitemap_url_scheme = '{link}'
 
 # Add any paths that contain templates here, relative to this directory.
